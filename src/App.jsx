865cdbcfda3c1c3c7278c22dc64ec3200e685bd5
ruainpro <ruainpro@gmail.com>
2025-07-01 00:05:03 -0400

    fixes done

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,10 @@ function App() {
               <UserDashboardHome />
             </PrivateRoute>
           }
-        />
+        >
+          <Route path="action-history" element={<ActionHistoryPage />} />
+
+          </Route>
 
         {/* User Account Pages */}
         <Route
